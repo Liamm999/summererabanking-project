2f41d4e3cede014708cbb445c2d522e9143dd0ec
unknown <lelam96705@gmail.com>
2023-02-20 15:47:05 +0700

    second commit

--- src/admin/router.js
+++ src/admin/router.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 export default [
   {
-    path: "/admin", // route
+    path: "/demo", // route
     name: "admin", // name of the views page
     component: () => import("./views/demo.vue"), // directory of vue file path
     meta: {
