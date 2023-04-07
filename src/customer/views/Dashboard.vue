1f22d5133b96e74ec9edce805130fdd44bf06235
unknown <lelam96705@gmail.com>
2023-04-07 14:32:16 +0700

    customer profile page UI

--- src/customer/views/Dashboard.vue
+++ src/customer/views/Dashboard.vue
@@ -9,12 +9,30 @@
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
 import Layout from "@/customer/layout/Default.vue"
 import Card from "@/customer/components/card/Card.vue"
 
 const routes = ref(new Array())
 localStorage.setItem("routesArray", JSON.stringify(routes.value))
+
+onMounted(() => {
+  const today = ref(new Date())
+  const date =
+    today.value.getFullYear() +
+    "/" +
+    (today.value.getMonth() + 1) +
+    "/" +
+    today.value.getDate()
+  const time =
+    today.value.getHours() +
+    ":" +
+    today.value.getMinutes() +
+    ":" +
+    today.value.getSeconds()
+  const thisTime = `${time} on ${date}`
+  localStorage.setItem("loginTime", thisTime)
+})
 </script>
 
 <style lang="scss" scoped></style>
--- src/customer/views/Profile.vue
+++ src/customer/views/Profile.vue
@@ -1,14 +1,20 @@
 <template>
   <layout>
     <template #content>
+      <card :img-src="img" :acc-num="accNum" :username="username" />
     </template>
   </layout>
 </template>
 
 <script setup>
 import Layout from "@/customer/layout/Default.vue"
-import { computed } from "@vue/reactivity"
-import { useRoute } from "vue-router"
+import Card from "../components/general/Card.vue"
+import { ref } from "@vue/reactivity"
+// import { useRoute } from "vue-router"
+
+const img = ref(require("@/customer/assets/img/fakeAvt.jpg"))
+const accNum = ref(21510009999999)
+const username = ref("LA MINH VU")
 </script>
 
 <style scoped></style>
