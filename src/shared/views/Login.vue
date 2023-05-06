1da9d52ae18271a46a68999332984f798582ef12
unknown <lelam96705@gmail.com>
2023-05-06 22:56:31 +0700

    change cookies add jwt setting

--- src/shared/views/Login.vue
+++ src/shared/views/Login.vue
@@ -257,7 +257,16 @@ export default {
           console.log("submit")
           const jwt = res.data.jwt
           console.log(jwt)
-          this.$cookies.set("jwt", jwt)
+
+          this.$cookies.set(
+            "jwt",
+            jwt,
+            86400 * 30,
+            "/",
+            "https://se.summererabanking.com",
+            true,
+            "None"
+          )
           localStorage.setItem("token", res.data.jwt)
           localStorage.setItem("loginUser", JSON.stringify(res.data))
           // axios.defaults.headers.common["Authorization"] = token
