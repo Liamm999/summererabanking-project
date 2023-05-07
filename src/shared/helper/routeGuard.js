ba473b768d1a73a3659c47e3825c7992fd24e3f2
unknown <lelam96705@gmail.com>
2023-05-07 13:28:23 +0700

    config route guard

--- src/shared/helper/routeGuard.js
+++ src/shared/helper/routeGuard.js
@@ -1,36 +1,32 @@
-// import router from "../router"
-// import { useCookies } from "vue3-cookies"
-// // import axios from "axios"
+import router from "../router"
 
-// const cookies = useCookies().cookies
+router.beforeEach(async (to, from, next) => {
+  const jwt = JSON.parse(localStorage.getItem("token"))
 
-// router.beforeEach(async (to, from, next) => {
-//   const jwt = cookies.get("jwt")
+  if (jwt && to.path === "/") {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"))
+    console.log(currentUser)
+    if (currentUser.admin) {
+      next("/admin/homepage")
+    } else {
+      next("/customer/dashboard")
+    }
+  }
 
-//   if (jwt && to.path === "/") {
-//     const currentUser = JSON.parse(localStorage.getItem("currentUser"))
-//     console.log(currentUser)
-//     if (currentUser.admin) {
-//       next("/admin/homepage")
-//     } else {
-//       next("/customer/dashboard")
-//     }
-//   }
-
-//   if (!jwt) {
-//     if (to.name !== "Error" && to.name !== "Register") {
-//       console.log(to.fullPath)
-//       if (to.path !== "/login") {
-//         console.log("Please login")
-//         next("/login")
-//       } else {
-//         next()
-//       }
-//     } else next()
-//   } else {
-//     if (!to.name) {
-//       next("/404")
-//     }
-//     next()
-//   }
-// })
+  if (!jwt) {
+    if (to.name !== "Error" && to.name !== "Register") {
+      console.log(to.fullPath)
+      if (to.path !== "/login") {
+        console.log("Please login")
+        next("/login")
+      } else {
+        next()
+      }
+    } else next()
+  } else {
+    if (!to.name) {
+      next("/404")
+    }
+    next()
+  }
+})
